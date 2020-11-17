33e09f11df360f3daf6cd4443518b8bc2df3bb77
truizdiaz <tomiruizdiaz@gmail.com>
2020-11-17 15:59:06 -0300

    Vue

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,18 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <Piano></Piano>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Piano from './components/Piano';
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    Piano
   }
 }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+
 </style>
---
+++ src/components/Piano.vue
@@ -0,0 +1,39 @@
+<template>
+  <div class="row">
+    <div class="col-lg-6 offset-lg-3">
+      <ul class="list-group">
+        <li v-for="(sonido, index) in sonidos" v-bind:key="index" 
+            class="list-group-item" v-bind:style="{ 'background-color':sonido }" 
+            v-on:click="aplicarSonido(index+1)"></li>        
+      </ul>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+            sonidos: ['red', 'orange', 'yellow', 'green', 'teal', 'blue', 'purple']
+        }
+    },
+    methods: {
+        aplicarSonido(index) {
+            const audio = new Audio();
+            audio.src = 'assets/note' + index + '.wav';
+            audio.load();
+            audio.play();
+        }
+    },
+};
+</script>
+
+<style scoped>
+.list-group-item {
+    height: 80px;
+    cursor: pointer;
+}
+.list-group-item:active {
+    opacity: 0.7;
+}
+</style>
